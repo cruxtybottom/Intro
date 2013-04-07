089813d051dfb34ed01472983bd524d1754f0a53
cryoyaid <ismeetdhillon@yahoo.com>
2013-04-07 14:21:17 -0400

    second commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,7 @@
+class DemoController < ApplicationController
+  def hello
+  	@my_name = params[:name]
+  	@foo = params[:foo]
+  	#render :text => "hello, #{@name}!!"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntroToRails::Application.config.secret_token = '8f6f9fd07e71878e55ac05c8d4a0bc5bd3f5a74a38e56b97d4c03d86db659c2ce459b63087731104cc3d3d1e572161f72c029da10c71e73695c0181de8903e1c'
